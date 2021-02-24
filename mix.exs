41ad7c932d6b63a670559ffe9766ab5b9efdc86c
xinz <joy2everyone.z@gmail.com>
2021-02-24 17:28:19 +0800

    Set version as 0.0.1-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oasis.MixProject do
   def project do
     [
       app: :oasis,
-      version: "0.1.0",
+      version: "0.0.1-dev",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
