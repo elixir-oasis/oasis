9c79ac2190817b255ef39c355c7d9e80c18accd2
hou8 <kurt.hou@edragonconnect.com>
2021-04-21 11:39:07 +0800

    fix request validator trim as ... when too long

--- priv/templates/oas.gen.plug/plug/request_validator.exs
+++ priv/templates/oas.gen.plug/plug/request_validator.exs
@@ -3,7 +3,7 @@ plug(
   <%= [:query_schema, :cookie_schema, :header_schema, :body_schema] |> Enum.reduce([], fn(k, acc) ->
     value = Map.get(router, k)
     if value != nil do
-      ["#{k}: #{inspect(value)}" | acc]
+      ["#{k}: #{inspect(value, limit: :infinity)}" | acc]
     else
       acc
     end
