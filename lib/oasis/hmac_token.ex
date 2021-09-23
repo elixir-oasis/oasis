0b4efeb73071f9a29bb1affd4e64880d9df133b1
hou8 <houkun_3054@foxmail.com>
2021-09-23 15:37:21 +0800

    docs: add guide

--- lib/oasis/hmac_token.ex
+++ lib/oasis/hmac_token.ex
@@ -8,62 +8,64 @@ defmodule Oasis.HmacToken do
   * `c:crypto_configs/2`, provides a way to define the crypto-related key information for the high level usage,
     it required to return a list of `#{inspect(__MODULE__)}.Crypto` struct.
   * `c:verify/3`, an optional function to provide a way to custom the verification of the token, you may
-    want to validate request datetime, or other more rules to verify it.
+    want to validate request datetime, HTTP body or other more rules to verify it.
 
 
   ## Example
 
   Here is an example to verify that HTTP request time does not exceed the current time by 1 minute.
 
-      defmodule Oasis.Gen.HmacAuth do
-        @behaviour Oasis.HmacToken
-        alias Oasis.HmacToken.Crypto
+  ```elixir
+  defmodule Oasis.Gen.HmacAuth do
+    @behaviour Oasis.HmacToken
+    alias Oasis.HmacToken.Crypto
 
-        # in seconds
-        @max_diff 60
+    # in seconds
+    @max_diff 60
 
-        @impl true
-        def crypto_configs(_conn, _opts) do
-          [
-            %Crypto{
-              credential: "...",
-              secret: "..."
-            }
-          ]
+    @impl true
+    def crypto_configs(_conn, _opts) do
+      [
+        %Crypto{
+          credential: "...",
+          secret: "..."
+        }
+      ]
+    end
+
+    @impl true
+    def verify(conn, token, _opts) do
+      with {:ok, _} <- Oasis.HmacToken.verify_signature(conn, token, opts),
+           {:ok, timestamp} <- conn |> get_header_date() |> parse_header_date() do
+        timestamp_now = DateTime.utc_now() |> DateTime.to_unix()
+
+        if timestamp_now - timestamp < @max_diff do
+          {:ok, timestamp}
+        else
+          {:error, :expired}
         end
-
-        @impl true
-        def verify(conn, token, _opts) do
-          with {:ok, _} <- Oasis.HmacToken.verify_signature(conn, token, opts),
-               {:ok, timestamp} <- conn |> get_header_date() |> parse_header_date() do
-            timestamp_now = DateTime.utc_now() |> DateTime.to_unix()
-
-            if timestamp_now - timestamp < @max_diff do
-              {:ok, timestamp}
-            else
-              {:error, :expired}
-            end
-          end
-        end
-
-        defp get_header_date(conn) do
-          conn
-          |> Plug.Conn.get_req_header("x-oasis-date")
-          |> case do
-            [date] -> date
-            _ -> nil
-          end
-        end
-
-        defp parse_header_date(str) when is_binary(str) do
-          with {:ok, datetime} <- Timex.parse(str, "%a, %d %b %Y %H:%M:%S GMT", :strftime),
-               timestamp when is_integer(timestamp) <- Timex.to_unix(datetime) do
-            {:ok, timestamp}
-          end
-        end
-
-        defp parse_header_date(_otherwise), do: {:error, :expired}
       end
+    end
+
+    defp get_header_date(conn) do
+      conn
+      |> Plug.Conn.get_req_header("x-oasis-date")
+      |> case do
+        [date] -> date
+        _ -> nil
+      end
+    end
+
+    defp parse_header_date(str) when is_binary(str) do
+      with {:ok, datetime} <- Timex.parse(str, "%a, %d %b %Y %H:%M:%S GMT", :strftime),
+           timestamp when is_integer(timestamp) <- Timex.to_unix(datetime) do
+        {:ok, timestamp}
+      end
+    end
+
+    defp parse_header_date(_otherwise), do: {:error, :expired}
+  end
+  ```
   """
 
   defmodule Crypto do
